a542b92f45335288e9ee0459cb38f8c0ea0f68d6
Sergi Juanati <wint3rmute@gmail.com>
2022-09-07 18:20:05 +0200

    Updated parsers

---
+++ src/parsers/approval.ts
@@ -0,0 +1,20 @@
+// @ts-nocheck
+import { ApprovalEvent } from '../types/approval';
+
+//TODO: Careful, DAI has different fields!
+function parseApprovalEvent<T>(ev: T): ApprovalEvent {
+    const event = new ApprovalEvent(
+        ev.transaction.hash.toHex() + "-" + ev.logIndex.toString(),
+        ev.block.number,
+        ev.block.timestamp,
+        ev.address,
+        ev.params.owner.toHexString(),  // links with User.id
+        ev.params.spender,
+        ev.params.value,
+    )
+    return event;
+}
+
+export {
+    parseApprovalEvent
+}
---
+++ src/parsers/transfer.ts
@@ -0,0 +1,19 @@
+// @ts-nocheck
+import { TransferEvent } from '../types/transfer';
+
+function parseTransferEvent<T>(ev: T): TransferEvent {
+    const event = new TransferEvent(
+        ev.transaction.hash.toHex() + "-" + ev.logIndex.toString(),
+        ev.block.number,
+        ev.block.timestamp,
+        ev.address,
+        ev.params.from,
+        ev.params.to,
+        ev.params.value,
+    )
+    return event;
+}
+
+export {
+    parseTransferEvent
+}
